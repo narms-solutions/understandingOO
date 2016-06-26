4cbe99b35ca3aa180fe3187ffe551a1bf3f1dab0
narmada <narmada@kth.se>
2016-06-26 21:40:07 +0200

    Created New Class

---
+++ understandingOO/Auto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace understandingOO
+{
+    class Auto
+    {
+        public int Speed;
+        public int Year;
+        public int Miles;
+        private string make;
+        public string Model;
+        public string Color;
+
+        public string Maker
+        {
+            get { return make ; }
+            set { make = value; }
+        }
+
+        public int Accelerator(int incrementedSpeed)
+        {
+            Speed +=incrementedSpeed;
+            //Console.WriteLine("the current speed:" + speed);
+            return Speed;
+
+        }
+        public int Decelerator(int decrementedSpeed)
+        {
+            Speed -=decrementedSpeed;
+            //Console.WriteLine("The current speed:" +speed);
+            return Speed;
+        }
+        public string speedLimitViolation(int speedLimit,string initialMessage)
+        {
+            string message = "";
+            if (Speed > speedLimit)
+               // initialMessage = "warning!";
+                message = initialMessage+ "Too Fast!";
+            else
+                message = "No worries !! It's ok!";
+            //initialMessage = "No worries!";
+            return message;
+            
+                
+        }
+    }
+}
---
+++ understandingOO/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace understandingOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Auto myCar = new Auto();
+            myCar.Speed = 0;
+            myCar.Model = "SwiftDezire";
+            myCar.Year = 2009;
+            myCar.Maker = "oldsMobile";
+            myCar.Miles = 50000;
+            myCar.Color = "Silver";
+            Console.WriteLine(myCar.Color  + "  "  + myCar.Model);
+
+            int myCurrentSpeeed = 0;
+            myCurrentSpeeed= myCar.Accelerator(3);
+            myCar.Accelerator(6);
+            myCar.Accelerator(5);
+            myCar.Accelerator(110);
+            myCurrentSpeeed = myCar.Accelerator(28);
+            Console.WriteLine("The Current speed of the car is: " + myCurrentSpeeed);
+
+            string message = myCar.speedLimitViolation(10,"Warning!");
+         
+            Console.Beep();
+
+            Console.WriteLine(message);
+           //Console.WriteLine(message1);
+            myCurrentSpeeed = myCar.Decelerator(4);
+            Console.WriteLine("The reduced  speed of the car is: " + myCurrentSpeeed);
+            
+            Console.ReadLine();
+        }
+    }
+}
